f18a894e2be2d5f8105ec2649b0101015dd23812
makkei0526 <makkei1710@gmail.com>
2020-04-15 12:59:17 +0900

    gem rails-i18nを追加　エラーメッセージを日本語化

--- Gemfile
+++ Gemfile
@@ -63,5 +63,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # 追加gem
 gem 'haml-rails'
+gem 'rails-i18n'
 
 
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module Takefood
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # 表示TimeZone
+    config.time_zone = 'Tokyo'
+
+    # DB保存時間をlocal(Tokyo)にする
+    config.active_record.default_timezone = :local
+
+    # i18n
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
