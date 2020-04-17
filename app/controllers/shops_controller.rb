6b97b9c9408d2494cfc3a1a014bc1fe63a70664b
makkei0526 <makkei1710@gmail.com>
2020-04-17 16:21:12 +0900

    お店の情報を更新できるように実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -9,6 +9,12 @@ class ShopsController < ApplicationController
     @shop = Shop.find(params[:id])
   end
 
+  def update
+    @shop = Shop.find(params[:id])
+    @shop.update(shop_params)
+    redirect_to shop_path(@shop.id), notice: 'お店情報を更新しました'
+  end
+
   private
   def shop_params
     params.require(:shop).permit(:name,:image,:address,:introduction,:area,:shop_holiday,:shop_time)
