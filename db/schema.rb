0fcc64896120cba7098782cd4ad334f1f116076d
makkei0526 <makkei1710@gmail.com>
2020-04-17 19:39:59 +0900

    テイクアウト最新情報のためのpostモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_074106) do
+ActiveRecord::Schema.define(version: 2020_04_17_103657) do
 
   create_table "foods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2020_04_17_074106) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["shop_id"], name: "index_foods_on_shop_id"
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content", null: false
+    t.bigint "shop_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_posts_on_shop_id"
   end
 
   create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -42,4 +50,5 @@ ActiveRecord::Schema.define(version: 2020_04_17_074106) do
   end
 
   add_foreign_key "foods", "shops"
+  add_foreign_key "posts", "shops"
 end
