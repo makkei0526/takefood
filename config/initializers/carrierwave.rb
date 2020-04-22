cd2c4877eb518cff32d8286573485ea8f82ab972
makkei0526 <makkei1710@gmail.com>
2020-04-22 22:22:39 +0900

    プロバイダーのコメントアウト

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ require 'carrierwave/storage/fog'
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      provider: 'AWS',
+      # provider: 'AWS',
       aws_access_key_id:      ENV.fetch('AWS_ACCESS_KEY_ID'),
       aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       region: ENV.fetch('AWS_REGION') # Required
