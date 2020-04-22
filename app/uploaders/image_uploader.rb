e06c3e405844409d4db3e28ff87efe4cefc75054
makkei0526 <makkei1710@gmail.com>
2020-04-22 22:18:57 +0900

    S3の画像テスト

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,13 +1,13 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  if Rails.env.development?
+  # if Rails.env.development?
+  #   storage :fog
+  # elsif Rails.env.test?
+  #   storage :fog
+  # else
     storage :fog
-  elsif Rails.env.test?
-    storage :fog
-  else
-    storage :fog
-  end
+  # end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
